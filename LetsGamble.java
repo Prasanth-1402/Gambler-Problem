1403446f97d366d2f934a2efccc5d253c865fbe3
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-08-19 18:38:31 +0530

    [Prasanth] Calculated Gain/Loss on 20th day

--- LetsGamble.java
+++ LetsGamble.java
@@ -2,22 +2,32 @@ import java.util.Random;
 class LetsGamble {
 	public static void main(String args[]) {
 		int initialAmount = 100;
+		int currentAmount = initialAmount;
 		int betAmount = 1;
 
 		Random random = new Random();
 		int gambleResult = 0;
 		int isWinner = 0;
 		int isLoser = 1;
+		int dayCount=0;
 		while(initialAmount < 150  && initialAmount > 50){
 			gambleResult = random.nextInt(2);
-			if(gambleResult==isWinner)
-		   	initialAmount+=betAmount;
-			else
-			   initialAmount-=betAmount;
+			if(gambleResult==isWinner){
+		   	currentAmount+=betAmount;
+				dayCount++;
+			}
+
+			else{
+			   currentAmount-=betAmount;
+				dayCount++;
+			}
+
+			if(dayCount==20){
+				if(initialAmount>currentAmount)
+					System.out.println(initialAmount - currentAmount + " lost  after 20 days");		
+				else
+               System.out.println(currentAmount - initialAmount + " won  after 20 days");
+			}						
 		}
-		if(initialAmount>100)
-			System.out.println("IN THE END IT IS : "+ initialAmount +" - AND SO IT IS A WIN");		
-		else
-			System.out.println("IN THE END IT IS : "+initialAmount + " - AND  SO IT IS A LOSS");
 	}
 }
