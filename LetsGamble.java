0a7c0e02a196c9b8be16d4fbe6bff88a70b7c220
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-08-19 15:45:06 +0530

    [Prasanth] Checking for a Win/Loss

--- LetsGamble.java
+++ LetsGamble.java
@@ -1,7 +1,16 @@
+import java.util.Random;
 class LetsGamble {
 	public static void main(String args[]) {
 		int initialAmount = 100;
 		int betAmount = 1;
-		System.out.println(initialAmount +" "+betAmount);
+
+		Random random = new Random();
+		int gambleResult = random.nextInt(2);
+		int isWinner = 0;
+		int isLoser = 1;
+		if(gambleResult == isWinner)
+			System.out.println("HURRAY!!! ITS A WIN");
+		else
+			System.out.println("BETTER LUCK NEXT TIME.!");
 	}
 }
